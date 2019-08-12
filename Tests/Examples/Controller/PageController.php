43e5cc54826e60c18bf3ef17e0d14a2c6461baa1
Cornelius K <kahns@bdrops.de>
2019-08-12 12:08:45 +0200

    Updated tests

--- Tests/Examples/Controller/PageController.php
+++ Tests/Examples/Controller/PageController.php
@@ -9,25 +9,22 @@ use RevisionTen\CQRS\Services\CommandBus;
 use RevisionTen\CQRS\Tests\Examples\Command\PageCreateCommand;
 use RevisionTen\CQRS\Tests\Examples\Model\Page;
 use Ramsey\Uuid\Uuid;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class PageController extends Controller
+class PageController extends AbstractController
 {
     public function createPage(CommandBus $commandBus, AggregateFactory $aggregateFactory, string $title)
     {
         $aggregateUuid = Uuid::uuid1()->toString();
 
-        $success = false;
-        $successCallback = static function ($commandBus, $event) use (&$success) { $success = true; };
-
         $pageCreateCommand = new PageCreateCommand(1, null, $aggregateUuid, 0, [
             'title' => $title,
-        ], $successCallback);
+        ]);
 
         // Execute Command.
-        $commandBus->dispatch($pageCreateCommand);
+        $success = $commandBus->dispatch($pageCreateCommand);
 
         if (!$success) {
             return new Response('fail', 500);
