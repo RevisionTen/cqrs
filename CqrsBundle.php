82dc756ef0d9c6c83b67bdbdbde5f81debeaa739
Cornelius K <kahns@bdrops.de>
2019-10-17 15:05:24 +0200

    Bugfix for commandbus execute qeued use

--- CqrsBundle.php
+++ CqrsBundle.php
@@ -11,7 +11,7 @@ use function defined;
 
 class CqrsBundle extends Bundle
 {
-    public const VERSION = '2.0.0';
+    public const VERSION = '2.0.1';
 
     private function setConstants(): void
     {
